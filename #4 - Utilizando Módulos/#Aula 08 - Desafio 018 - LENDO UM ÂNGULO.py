7f0132968090beaff6293e93919bd3437a2d69a6
Yago Andrade <yagoandradelima@outlook.com>
2022-02-25 20:32:31 -0300

    commit de novas atividades

--- #4 - Utilizando Módulos/#Aula 08 - Desafio 018 - LENDO UM ÂNGULO.py
+++ #4 - Utilizando Módulos/#Aula 08 - Desafio 018 - LENDO UM ÂNGULO.py
@@ -1,9 +1,11 @@
 #Aula 08 - Desafio 018 - LENDO UM ÂNGULO
 
-import math
+from math import radians, sin, cos, tan
 
 ang = int(input('Digite o ângulo: '))
-seno = math.sin(ang)
-coss = math.cos(ang)
+#As funções _sin_ e _cos_ para funcionar perfeitamente precisam ter a variável de cálculo convertida para radianos
+seno = sin(radians(ang))
+coss = cos(radians(ang))
+tg = tan(radians(ang))
 
-print(f'O seno de {ang} é {math.ceil(seno)} e o cosseno é {math.ceil(coss)}')
+print(f'O seno de {ang} é {seno:.2f}. O cosseno é {coss:.2f}. E a tangente {tg:.2f}')
--- #4 - Utilizando Módulos/#Aula08 - Desafio 016 - QUEBRANDO UM NÚMERO.py
+++ #4 - Utilizando Módulos/#Aula08 - Desafio 016 - QUEBRANDO UM NÚMERO.py
@@ -6,3 +6,6 @@ num = float(input('Digite um número inteiro real acima de 3 casas após a vírg
 #A função trunc elimina da vírgula pra frente, deixando só a parte real
 vis = math.trunc(num)
 print(vis)
+
+#O Truncate quebra o número na parte inteira. No caso desse desafio, se fosse da minha preferência
+#eu poderia simplesmente ter utilizado o _int_ no lugar do truncate
